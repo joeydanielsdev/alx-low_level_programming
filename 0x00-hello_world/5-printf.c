d2a164192c2ba811868689b354fe6f54909d8fdf
joeydaniesl <jonathanezeh57h57@gmail.com>
2022-09-11 14:34:22 -0700

    Learning C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 /**
- *
- *  * main - Prints output
+ * main - Prints output
  *
  * Return: Always 0
  *
  */
 
 int main(void)
-
 {
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
