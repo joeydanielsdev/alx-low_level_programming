6525ed48e96075ea89be841f8b61a6f09dc0066e
joeydaniesl <jonathanezeh57h57@gmail.com>
2022-09-11 11:16:21 -0700

    Learning C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
  #include <stdio.h>
  /** print text without the print function **/
 
- int main(void)
+ int main()
 
 {
-	puts("Programming is like building a multilingual puzzle"/n)
+	puts("Programming is like building a multilingual puzzle\n");
 	return 0;
 }
