7846e5e73692544dbd5088aac0b7d838cc6de497
joeydaniesl <jonathanezeh57h57@gmail.com>
2022-09-11 12:09:36 -0700

    Learning C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
  #include <stdio.h>
  /**
   *
-  ** main - Entry point
+  * main - Entry point
   *
   ** Description: 'program to print text without prinf function'
   *
   ** Return: Always 0 (Success)
   *
   */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
